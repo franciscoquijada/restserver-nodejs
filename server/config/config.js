a30f702206f700f9f27db8b4d3f960a03cc5c251
fquijada <fquijada@celcom.cl>
2020-08-16 22:35:41 -0400

    Agregado en config para poder cambiar entre conexion a bd en mongo atlas y en local

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,17 @@
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno 
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+let UrlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    UrlDB = 'mongodb://localhost:27017/cafe';
+} else{
+    UrlDB = 'mongodb+srv://fquijada:cKObkufeIr5tS3Z9@cluster0.xeuhb.mongodb.net/cafe';
+}
+
+process.env.URLDB = UrlDB;
+
