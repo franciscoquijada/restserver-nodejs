7d807f011b9aa2804bf4caf75346210fc7fa6d6a
fquijada <fquijada@celcom.cl>
2020-08-25 22:56:45 -0400

    Creado modelo Producto

---
+++ server/models/producto.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var productoSchema = new Schema({
+    nombre: {type: String, required: [true, "El nombre es obligatorio"]},
+    precioUnitario: {type: double, required: [true, "El precio es obligatorio"]},
+    descripcion: {type: String, required: false},
+    disponible: {type: Boolean, required: true, default: true},
+    categoria: {type: Schema.Types.ObjectId, ref: 'Categoria', required: true},
+    usuario: {type: Schema.Types.ObjectId, ref: 'Usuario'}
+});
+
+module.exports = mongoose.model('Producto', productoSchema);
